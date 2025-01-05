62fabfc6a6960c70100dd910e5af7b5d3e308366
VictorSDS2801 <victordiogo2801@gmail.com>
2025-01-05 14:36:43 -0300

    Add files via upload

---
+++ calculadora.py
@@ -0,0 +1,22 @@
+#calculadora ;-;
+#acho que consigo fazer
+#lesgoo
+
+print("calculadora")
+print("use + para adiçao, - para subtraçao, * para multiplicaçao e / para divisao")
+numero1 = int(input("digite seu primeiro numero"))
+operaçao = input("digite qual operaçao voce quer fazer")
+numero2 = int(input("digite seu segundo numero"))
+
+
+
+#adiçao
+if operaçao == "+":
+    print(numero1 + numero2)
+elif operaçao == "-":
+    print(numero1 - numero2)
+elif operaçao == "*":
+    print(numero1 * numero2)
+elif operaçao == "/":
+    print(numero1 / numero2)
+
