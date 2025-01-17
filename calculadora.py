a38402ad90d9f0d42425002adcd3bb844db6368a
Victor Diogo <victordiogo2801@gmail.com>
2025-01-17 12:32:41 -0300

    Update calculadora.py

--- calculadora.py
+++ calculadora.py
@@ -13,10 +13,13 @@ numero2 = int(input("digite seu segundo numero"))
 #adiçao
 if operaçao == "+":
     print(numero1 + numero2)
+#subtraçao
 elif operaçao == "-":
     print(numero1 - numero2)
+#multiplicaçao
 elif operaçao == "*":
     print(numero1 * numero2)
+#divisao
 elif operaçao == "/":
     print(numero1 / numero2)
 
